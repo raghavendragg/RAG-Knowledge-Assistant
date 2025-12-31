51b3027699ed246950ae020743312d9ba112b248
Raghavendra G G <gg.raghav@gmail.com>
2025-12-31 15:04:58 +0530

    updating repo

--- config/settings.py
+++
@@ -1,4 +0,0 @@
-class Settings:
-    DATA_PATH: str = "/Users/raghavendraggoudar/Desktop/AI-MATERIALS/RAG/rag_app/data/documents"
-    EMBEDDINGS_MODEL_NAME: str = 'sentence-transformers/all-MiniLM-L6-v2'
-    TOP_K: int = 5
